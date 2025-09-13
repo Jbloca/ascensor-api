1b5fc8b1bf07a8a680d2020ee0981ecd1275a10b
Jorge Balarezo <soporteti@intercertlatam.com>
2025-09-13 16:25:49 -0500

    Fix: Configurar puerto 10000 para Render

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const cardRoutes = require('./routes/cards');
 const elevatorRoutes = require('./routes/elevator');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware de seguridad
 app.use(helmet());
